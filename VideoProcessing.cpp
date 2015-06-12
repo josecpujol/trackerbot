0d3ca0c5a31372bf85f396641d0e092ce49fc693
Jose Carlos Pujol <jpujol@tokbox.com>
2015-06-12 16:38:15 -0700

    Added test marker. Playing with feature detection

--- VideoProcessing.cpp
+++ VideoProcessing.cpp
@@ -1,4 +1,7 @@
 #include "opencv2/opencv.hpp"
+#include <iostream>
+
+const char marker_file[] = "./marker.png";
 
 using namespace cv;
 
@@ -8,17 +11,48 @@ int main(int, char**)
     if(!cap.isOpened())  // check if we succeeded
         return -1;
 
-    Mat edges;
-    namedWindow("edges",1);
+    // Read marker and detect features
+    cv::Mat marker = cv::imread(marker_file, CV_LOAD_IMAGE_GRAYSCALE );
+    if( !marker.data) {
+      std::cout<< " Error reading marker: " << marker_file << std::endl;
+      return -1;
+    }
+
+    FastFeatureDetector detector(10, false);
+    std::vector<KeyPoint> keypoints_1;
+    detector.detect(marker, keypoints_1 );
+    std::cout << "Num keypoints: " << keypoints_1.size() << std::endl;
+    std::vector<Point2f> raw_keypoints_marker, filtered_keypoints_marker;
+    KeyPoint::convert(keypoints_1, raw_keypoints_marker);
+
+    // Remove those close to each other
+    for (auto point : raw_keypoints_marker) {
+
+    }
+
+
+    Mat img_keypoints_1;
+
+    drawKeypoints( marker, keypoints_1, img_keypoints_1, Scalar::all(-1), DrawMatchesFlags::DEFAULT );
+    imshow("Keypoints 1", img_keypoints_1 );
+
+    cv::waitKey();
+    return 0;
+
+    cv::namedWindow("edges",1);
     for(;;)
     {
-        Mat frame;
-        cap >> frame; // get a new frame from camera
-        cvtColor(frame, edges, CV_BGR2GRAY);
-//        GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5);
-        Canny(edges, edges, 0, 30, 3);
-        imshow("edges", edges);
-        if(waitKey(30) >= 0) break;
+      Mat image;
+      cv::Mat frame;
+      cap >> frame; // get a new frame from camera
+      cv::cvtColor(frame, frame, CV_BGR2GRAY);
+      detector.detect(frame, keypoints_1 );
+      //        std::cout << "Num keypoints: " << keypoints_1.size() << std::endl;
+      drawKeypoints( frame, keypoints_1, img_keypoints_1, Scalar::all(-1), DrawMatchesFlags::DEFAULT );
+
+
+      cv::imshow("edges", img_keypoints_1);
+      if(cv::waitKey(30) >= 0) break;
     }
     // the camera will be deinitialized automatically in VideoCapture destructor
     return 0;
